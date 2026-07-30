84f86003f75e009e32795c803f475cb824ea8468
castanha7 <vinicius.calegari@escola.pr.gov.br>
2026-07-30 12:03:18 -0300

    ss

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ let numero2 = 5
 
 console.log("Adição: " + (numero1 + numero2)) // 15
 console.log("Subtração: " + (numero1 - numero2)) // 5
-console.log("Multiplicação
+console.log("Multiplicação: " + (numero1 * numero2))
+console.log("divisão: " + (numero1 / numero2)) // 2
+console.log("Módulo: " + (numero1 % numero2)) //0
+
